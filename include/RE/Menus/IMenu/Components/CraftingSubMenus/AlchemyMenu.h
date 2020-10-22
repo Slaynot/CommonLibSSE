0abe03bcc0803b7dfb7e6fa6ce7fd181245bd29b
Slaynot <slaynot@zaclys.net>
2020-10-22 21:27:47 +0200

    - Documenting AlchemyMenu a bit more

--- include/RE/Menus/IMenu/Components/CraftingSubMenus/AlchemyMenu.h
+++ include/RE/Menus/IMenu/Components/CraftingSubMenus/AlchemyMenu.h
@@ -35,29 +35,32 @@ namespace RE
 
 			class UsableEffectMap
 			{
-				std::uint32_t ingredientIndex; // 00
-				std::uint32_t effectIndex;     // 04
-				std::uint32_t unk8;            // 06
+			public:
+				std::uint32_t ingredientIndex;              // 00
+				std::uint32_t effectIndex;                  // 04
+				std::uint32_t nextCompatibleEffectMapIndex; // 06
 			};
 			static_assert(sizeof(UsableEffectMap) == 0xC);
 
 			class MenuIngredientEntry
 			{
-				RE::IngredientItem *ingredient;      // 00
-				std::uint8_t		effect1FilterID; // 09
-				std::uint8_t		effect2FilterID; // 0A
-				std::uint8_t		effect3FilterID; // 0B
-				std::uint8_t		effect4FilterID; // 0C
-				std::uint8_t		isSelected;      // 0D
-				std::uint8_t		maybeCompatible; // 0E
-				std::uint16_t		padE;            // 0F
+			public:
+				RE::InventoryEntryData *ingredient;     // 00
+				std::uint8_t		   effect1FilterID; // 09
+				std::uint8_t		   effect2FilterID; // 0A
+				std::uint8_t		   effect3FilterID; // 0B
+				std::uint8_t		   effect4FilterID; // 0C
+				std::uint8_t		   isSelected;      // 0D
+				std::uint8_t		   isNotGreyed;     // 0E
+				std::uint16_t		   padE;            // 0F
 			};
 			static_assert(sizeof(MenuIngredientEntry) == 0x10);
 
 			class PotionCreationData
 			{
-				BSTArray<UsableEffectMap*>      usableEffectsMaps; // 0
-				BSTArray<MenuIngredientEntry*>* ingredientEntries; // 18
+			public:
+				BSTArray<UsableEffectMap>      usableEffectsMaps; // 0
+				BSTArray<MenuIngredientEntry>* ingredientEntries; // 18
 			};
 			static_assert(sizeof(PotionCreationData) == 0x20);
 
@@ -69,17 +72,17 @@ namespace RE
 
 
 			// members
-			BSTArray<void*>    unk100;	               // 100
-			BSTArray<void*>    unk118;	               // 118
-			std::uint64_t	   unk130;	               // 130
-			PotionCreationData potionCreationData;     // 138
-			GFxValue		   unk158;	               // 158
-			std::uint64_t	   unk170;	               // 170
-			TESForm*		   unk178;	               // 178
-			TESForm*		   unk180;	               // 180
-			std::uint64_t	   unk188;	               // 188
-			BSFixedString	   newRecipeFormatString;  // 190
-			std::uint64_t	   playerHasPurityPerk;	   // 198
+			BSTArray<MenuIngredientEntry> ingredientEntries;     // 100
+			BSTArray<void*>               unk118;	             // 118
+			std::uint64_t	              unk130;	             // 130
+			PotionCreationData            potionCreationData;    // 138
+			GFxValue		              unk158;	             // 158
+			std::uint64_t	              unk170;	             // 170
+			TESForm*		              unk178;	             // 178
+			TESForm*		              unk180;	             // 180
+			std::uint64_t	              unk188;	             // 188
+			BSFixedString	              newRecipeFormatString; // 190
+			std::uint64_t	              playerHasPurityPerk;	 // 198
 		};
 		static_assert(sizeof(AlchemyMenu) == 0x1A0);
 	}
--- include/RE/Misc/Misc.h
+++ include/RE/Misc/Misc.h
@@ -17,7 +17,6 @@ namespace RE
 	void	 CreateRefHandle(RefHandle& a_handleOut, TESObjectREFR* a_refrTo);
 	void	 DebugNotification(const char* a_notification, const char* a_soundToPlay = 0, bool a_cancelIfAlreadyQueued = true);
 	float    GetArmorFinalRating(InventoryEntryData *a_entryData, float a_armorPerks, float a_skillMultiplier);
-	void	 GetCompatibleEffects(PotionCreationData* a_potionCreationData, BSTArray<EffectSetting*>* a_compatibleEffects, bool a_unk, bool HasPurityPerk);
 	Setting* GetINISetting(const char* a_name);
 	bool	 LookupReferenceByHandle(const RefHandle& a_handle, NiPointer<Actor>& a_refrOut);
 	bool	 LookupReferenceByHandle(const RefHandle& a_handle, NiPointer<TESObjectREFR>& a_refrOut);
